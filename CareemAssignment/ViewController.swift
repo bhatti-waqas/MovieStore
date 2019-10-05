fab0beae24a52afcd3aaaa1af1a2b2288e040a00
Waqas Naseem <waqas.naseem@starzplayarabia.com>
2019-10-06 00:22:00 +0500

    Initial Commit

---
+++ CareemAssignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CareemAssignment
+//
+//  Created by Waqas Naseem on 06/10/2019.
+//  Copyright © 2019 Waqas Naseem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
